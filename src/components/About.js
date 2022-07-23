409c405c477b98570a1c7733b72a1cb92a27a89e
Duc Nguyen <duc.dustinnguyen@gmail.com>
2022-07-23 13:29:21 -0400

    add profie pic, delete logo img

--- src/components/About.js
+++ src/components/About.js
@@ -36,8 +36,8 @@ export default function About() {
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            alt="profie-pic"
+            src="./profie.jpg"
           />
         </div>
       </div>
